cf4d6e701e7d439b69145de4d7b41a52323e6a53
Jada Arnett <jada.arnett@gmail.com>
2020-11-02 22:44:48 -0500

    adding footer component

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Wrapper from "./components/Wrapper/Wrapper";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import About from './pages/About';
 import Projects from './pages/Projects'
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route exact path="/about" component={About} />
           <Route exact path="/projects" component={Projects} />
         </Wrapper>
+        <Footer/>
       </div>
     </Router>
   );
