5a9438224816a42c57c7f9dd37307920873d259f
Jada Arnett <jada.arnett@gmail.com>
2020-11-02 22:07:57 -0500

    adding web vitals back to client folder

---
+++ client/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import './NavBar.css';
+import { Link } from "react-router-dom";
+
+
+
+const NavBar = () => {
+  return (
+    <nav>
+      <div class="nav-wrapper">
+        <a href="#" class="brand-logo">
+          <img src="./logo.jpg" alt="logo" width="20" height="20"/>
+        </a>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li className="nav-item">
+            <Link
+              to="/"
+              className={
+                window.location.pathname === "/" || window.location.pathname === "/about"
+                  ? "nav-link active"
+                  : "nav-link"
+              }
+            >
+              About
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              to="/projects"
+              className={
+                window.location.pathname === "/projects" || window.location.pathname === "/projects"
+                  ? "nav-link active"
+                  : "nav-link"
+              }
+            >
+              Projects
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
---
+++ client/src/pages/About.js
@@ -0,0 +1,53 @@
+import React from "react";
+
+function About() {
+  return (
+    <section className="hero">
+      <h1>
+        {" "}
+        Hello, my name is Jada. I am a Full Stack Web Developer living in
+        Atlanta, GA.
+      </h1>
+      <img></img>
+
+      <h5> About:</h5>
+      <p>
+        I am a MERN Stack Web Developer that has just completed a coding
+        bootcamp with the Georgia Institute of Technology. See my previous work
+        history by viewing my full resume here. Before I stepped into the
+        technology sector, I was an event coordinator for one of the most
+        popular restaurant groups in Atlanta. I am also a part time cardio
+        kickboxing instructor at the only female owned boxing gym in the
+        country!
+        <br />
+        <br />
+        When it comes to web development, I keep Michael Scott's "KISS: Keep it
+        Simple Stupid" at the front of my mind. I enjoy creating, clean, simple,
+        and straight to the point web applications. I am currently accepting
+        freelance work, so please contact me if you would like to build
+        something awesome!
+      </p>
+      <h5> Services:</h5>
+
+      <h6>Front End Web Development</h6>
+      <p>
+        Includes building the visual components of a website. I use HTML,
+        Javascript, and CSS to build visually appealing, responsive, interactive
+        websites.{" "}
+      </p>
+      <h6>Back End Web Development</h6>
+      <p>
+        Includes connecting the web page that is served up to the client to a
+        database that houses information. Focuses on building server-side web
+        application logic that integrates seamlessly with front end logic.
+      </p>
+      <hr/>
+      
+      <h2> Let's Connect!</h2>
+
+    </section>
+
+  );
+}
+
+export default About;
