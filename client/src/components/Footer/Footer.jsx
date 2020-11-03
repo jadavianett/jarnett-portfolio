3b3eb15ccdfc49711595741b3cff4946a67b4bc7
Jada Arnett <jada.arnett@gmail.com>
2020-11-02 22:45:09 -0500

    adding footer component

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './Footer.css';
+
+function Footer () {
+    return (
+        <footer class="page-footer">
+            <div class = "container">
+            copyright 2020 jada arnett
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
